f29dda0dde78a0aed3b9a5ea23c56eac21c17d58
Jeffy Lau <100321067+jeffy2560@users.noreply.github.com>
2022-04-01 13:40:03 -0700

    Clear input after submit

--- src/form.js
+++ src/form.js
@@ -14,6 +14,8 @@ class Form extends Component {
         evt.preventDefault();
         const addedId = {...this.state, id: uuidv4()}
         this.props.addItem(addedId);
+        this.setState({todo: ''});
+        
     }
     handlechange(evt){
         this.setState({
@@ -25,7 +27,7 @@ class Form extends Component {
         <div>
             <form onSubmit={this.handlesubmit}>
             <label htmlFor='newTodo'>New Item</label>
-            <input onChange={this.handlechange} id='newTodo' name='todo'></input>
+            <input onChange={this.handlechange} value={this.state.todo} id='newTodo' name='todo'></input>
             <button>Submit</button>
             </form>
         </div>
