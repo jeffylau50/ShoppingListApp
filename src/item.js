b61267faeb685887e1825f355184bf1b93138abe
Jeffy Lau <100321067+jeffy2560@users.noreply.github.com>
2022-04-01 14:35:25 -0700

    Full Crud updated

--- src/item.js
+++ src/item.js
@@ -4,21 +4,57 @@ class Item extends Component {
     constructor(props){
         super(props);
         this.state = {
-
+        editing: false, 
+        todo: ''
         };
         this.handleClick = this.handleClick.bind(this);
+        this.handleEdit = this.handleEdit.bind(this);
+        this.editButton = this.editButton.bind(this);
+        this.submitEdit = this.submitEdit.bind(this);
+        this.handlechange = this.handlechange.bind(this);
     }
+    submitEdit(evt){
+        evt.preventDefault();
+        this.setState({editing: false})
+        this.props.editItem(this.props.id, this.state.todo)
+    }
+    editButton(){
+        this.setState({editing: true})
+    }
+    handleEdit(id){
+        this.props.editItem(this.props.id, )
+      }
     handleClick(id){
         this.props.handleDelete(this.props.id)
         console.log(this.props.id)
     }
+    handlechange(evt){
+        this.setState({
+            [evt.target.name]: evt.target.value
+        })
+    }
     render(){
+        let result;
+        if(this.state.editing === true){
+            result = (
+                <div>
+                    <form>
+                    <input onChange={this.handlechange} name='todo' defaultValue={this.props.todob}></input>
+                    <button onClick={this.submitEdit}>Submit</button>
+                    </form>
+                </div>
+            )
+        }else {
+            result = (
+                <div>
+                <p>{this.props.todob}</p>
+                <button onClick={this.editButton}>Edit</button>
+                <button onClick={this.handleClick}>X</button>
+                </div>
+            )
+        }
         return(
-            <div>
-            <p>{this.props.todob}</p>
-            <button>Edit</button>
-            <button onClick={this.handleClick}>X</button>
-            </div>
+            result
         )
     }
 }
--- src/list.js
+++ src/list.js
@@ -10,7 +10,18 @@ class List extends Component {
         };
         this.addItem = this.addItem.bind(this);
         this.handleDelete = this.handleDelete.bind(this);
+        this.editItem = this.editItem.bind(this);
     }
+    editItem(id, newContent){
+        let editItem = this.state.todob.map(item => {
+            if (item.id === id) {
+                return { ...item, todo: newContent};
+            }
+            return item;
+        })
+        this.setState({ todob : editItem});
+    }
+
     handleDelete(id){
         let newarray = this.state.todob.filter(p => p.id !== id)
         this.setState({todob : newarray})
@@ -21,9 +32,9 @@ class List extends Component {
     render(){
         return(
             <div>
-                {this.state.todob.map(todo => <Item handleDelete={this.handleDelete} id={todo.id}  todob={todo.todo} />)}
+                {this.state.todob.map(todo => <Item editItem={this.editItem} handleDelete={this.handleDelete} id={todo.id}  todob={todo.todo} />)}
 
-                <Form addItem={this.addItem} />
+                <Form  addItem={this.addItem} />
             </div>
         )
     }
